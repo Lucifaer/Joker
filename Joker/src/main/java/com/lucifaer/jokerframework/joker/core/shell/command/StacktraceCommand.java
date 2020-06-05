a6a74696ca1bde6428b18fb2bd212b3d1146a3d0
Lucifaer <l.lucifaer@gmail.com>
2020-06-05 14:15:20 +0800

    修复`Stacktrace`循环依赖的问题

--- Joker/src/main/java/com/lucifaer/jokerframework/joker/core/shell/command/StacktraceCommand.java
+++ Joker/src/main/java/com/lucifaer/jokerframework/joker/core/shell/command/StacktraceCommand.java
@@ -2,6 +2,7 @@ package com.lucifaer.jokerframework.joker.core.shell.command;
 
 import com.lucifaer.jokerframework.joker.core.shell.ShellThrowableHandler;
 import org.jline.terminal.Terminal;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.commands.Stacktrace;
@@ -16,7 +17,7 @@ public class StacktraceCommand implements Stacktrace.Command {
     private final Terminal terminal;
     private final ShellThrowableHandler shellThrowableHandler;
 
-    public StacktraceCommand(Terminal terminal, ShellThrowableHandler shellThrowableHandler) {
+    public StacktraceCommand(@Lazy Terminal terminal, ShellThrowableHandler shellThrowableHandler) {
         this.terminal = terminal;
         this.shellThrowableHandler = shellThrowableHandler;
     }
